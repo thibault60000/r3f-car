651f2b451ec9018cc4886c9787aee53e55ef10e2
Thibault JP <thibault.jeanpierre@homepilot.fr>
2023-01-16 07:58:52 +0100

    update github link

--- src/Interface.js
+++ src/Interface.js
@@ -40,8 +40,8 @@ export default function Interface() {
       <div className='github'>
         <a
           target='_blank'
-          href='https://github.com/thibault60000'
-          alt='Lien vers profil Github : https://github.com/thibault60000'
+          href='https://github.com/thibault60000/r3f-car'
+          alt='Lien vers profil Github : https://github.com/thibault60000/r3f-car'
         >
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 496 512'>
             {/* Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
